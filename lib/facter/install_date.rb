dfcc017c71bcbf6758f7253740077f0735ee7a6f
beergeek <brett@puppet.com>
2018-07-31 16:33:54 +0800

    Fix ruby errors

--- lib/facter/install_date.rb
+++ lib/facter/install_date.rb
@@ -4,6 +4,7 @@ Facter.add('rhel_install_data') do
   confine :osfamily => 'RedHat'
   setcode do
     pre_output = Facter::Core::Execution.execute('rpm -qi basesystem | grep Install')
-    DateTime.parse(pre_output.gsub(%r{Install Date: }, '')).to_date
+    output = DateTime.parse(pre_output.gsub(%r{Install Date: }, '')).to_date
+    "#{output.year}_#{output.month}_#{output.day}"
   end
 end
